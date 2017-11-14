664b918dfc9394ad7cab539d3ebdbddbf0b71e34
littlexiaocai <13675150436cc@163.com>
2017-11-15 00:09:37 +0800

    cc1

--- ex1.py
+++
@@ -1,9 +0,0 @@
-
-print"Hello World"
-print"Hello Again"
-print"I like typing this "
-print"This is fun"
-print"Yay! pringting"
-print"I'd much rather you 'not'"
-print'I "said" do not touch this'# test
-
--- ex28.py
+++
@@ -1,5 +0,0 @@
-print "test" == "test"
-print "test" and "caicai"
-print "test" or "caicai"
-print 1==1 and 2==1
-
